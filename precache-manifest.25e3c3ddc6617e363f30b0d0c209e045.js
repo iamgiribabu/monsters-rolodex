9d5c3c415a6c4806eb76d3d3bfcd218dd7786027
iamgiribabu <girianisetti06@gmail.com>
2020-10-02 15:16:32 +0530

    Updates

---
+++ precache-manifest.25e3c3ddc6617e363f30b0d0c209e045.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eef015f31ce660e494f4e91bfc019a1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ccd7496e22452851c516",
+    "url": "/monsters-rolodex/static/css/main.25619923.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd7496e22452851c516",
+    "url": "/monsters-rolodex/static/js/main.369023e0.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
