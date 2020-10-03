dc59fc058a2f60a6416f039a28a431dcf521996a
iamgiribabu <girianisetti06@gmail.com>
2020-10-03 14:47:09 +0530

    corrected

--- src/App.js
+++ src/App.js
@@ -249,7 +249,7 @@ class App extends Component {
   componentDidMount(){
     fetch='https://jsonplaceholder.typicode.com/users'
     .then(response => response.json())
-    .then(users => this.setState({monsters = users}))
+    .then(users => this.setState({monsters : users}))
   }
   
   render(){
